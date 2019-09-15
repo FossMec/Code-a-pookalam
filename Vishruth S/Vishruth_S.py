148d6b78fab6b8a813dad03c753dbf73f6433fae
Vishruth-S <55310756+Vishruth-S@users.noreply.github.com>
2019-09-15 19:18:14 +0530

    Vishruth_S (#18)
    * Create readme.txt
    
    * Add files via upload

---
+++ Vishruth S/Vishruth_S.py
@@ -0,0 +1,262 @@
+#Code_a_Pookkalam by Vishruth_S
+
+import  turtle
+
+s=turtle.Turtle()
+s.speed(200)
+
+
+s.color('maroon','maroon')
+s.begin_fill()
+for i in range(12):
+    s.circle(150)
+    s.left(30)
+s.end_fill()
+
+s.left(45)
+
+s.color('brown','brown')
+s.begin_fill()
+for i in range(12):
+    s.circle(145)
+    s.left(30)
+s.end_fill()
+
+s.left(45)
+
+s.color('crimson','crimson')
+s.begin_fill()
+for i in range(12):
+    s.circle(140)
+    s.left(30)
+s.end_fill()
+
+s.left(45)
+
+s.color('orange','orange')
+s.begin_fill()
+for i in range(12):
+    s.circle(135)
+    s.left(30)
+s.end_fill()
+
+s.left(45)
+
+s.color('gold','gold')
+s.begin_fill()
+for i in range(12):
+    s.circle(130)
+    s.left(30)
+s.end_fill()
+
+s.left(45)
+
+s.color('yellow','yellow')
+s.begin_fill()
+for i in range(12):
+    s.circle(125)
+    s.left(30)
+s.end_fill()
+
+s.left(45)
+
+s.color('white','white')
+s.begin_fill()
+for i in range(12):
+    s.circle(120)
+    s.left(30)
+s.end_fill()
+
+s.color('darkgreen','darkgreen')
+s.begin_fill()
+s.forward(240)
+s.left(90)
+s.circle(240)
+s.end_fill()
+
+s.left(90)
+s.forward(240)
+s.color('orange','orange')
+s.begin_fill()
+for i in range(8):
+    s.left(15)
+    s.forward(240)
+    s.left(90)
+    s.circle(240,30)
+    s.left(90)
+    s.forward(240)
+s.end_fill()
+
+s.color('yellow','yellow')
+s.begin_fill()
+for i in range(8):
+    s.left(15)
+    s.forward(220)
+    s.left(90)
+    s.circle(220,30)
+    s.left(90)
+    s.forward(220)
+s.end_fill()
+
+s.color('white','white')
+s.begin_fill()
+for i in range(8):
+    s.left(15)
+    s.forward(200)
+    s.left(90)
+    s.circle(200,30)
+    s.left(90)
+    s.forward(200)
+s.end_fill()
+
+s.forward(180)
+s.left(90)
+s.color('green','green')
+s.begin_fill()
+s.circle(180)
+s.end_fill()
+
+s.left(90)
+s.forward(10)
+s.right(90)
+s.color('white','white')
+s.begin_fill()
+s.circle(170)
+s.end_fill()
+
+s.left(90)
+s.forward(170)
+s.color('purple','purple')
+
+
+s.begin_fill()
+for i in range(30):
+    s.left(12)
+    s.forward(120)
+    s.left(90)
+    s.forward(120)
+    s.left(90)
+    s.forward(120)
+    s.left(90)
+    s.forward(120)
+    s.left(90)
+s.end_fill()
+
+s.left(30)
+s.color('darkmagenta','darkmagenta')
+
+s.begin_fill()
+for i in range(30):
+    s.left(12)
+    s.forward(110)
+    s.left(90)
+    s.forward(110)
+    s.left(90)
+    s.forward(110)
+    s.left(90)
+    s.forward(110)
+    s.left(90)
+s.end_fill()
+
+
+s.left(30)
+s.color('magenta','magenta')
+
+s.begin_fill()
+for i in range(30):
+    s.left(12)
+    s.forward(100)
+    s.left(90)
+    s.forward(100)
+    s.left(90)
+    s.forward(100)
+    s.left(90)
+    s.forward(100)
+    s.left(90)
+s.end_fill()
+
+s.left(30)
+s.color('pink','pink')
+
+s.begin_fill()
+for i in range(30):
+    s.left(12)
+    s.forward(90)
+    s.left(90)
+    s.forward(90)
+    s.left(90)
+    s.forward(90)
+    s.left(90)
+    s.forward(90)
+    s.left(90)
+s.end_fill()
+
+s.left(30)
+s.color('lavender','lavender')
+
+s.begin_fill()
+for i in range(30):
+    s.left(12)
+    s.forward(85)
+    s.left(90)
+    s.forward(85)
+    s.left(90)
+    s.forward(85)
+    s.left(90)
+    s.forward(85)
+    s.left(90)
+s.end_fill()
+
+s.color('lightblue','lightblue')
+
+s.right(90)
+s.penup()
+s.forward(100)
+s.left(90)
+s.circle(100)
+s.pendown()
+for i in range(5):
+    s.begin_fill()
+    s.circle(50)
+    s.end_fill()
+    s.penup()
+    s.circle(100,70)
+    s.pendown()
+
+s.left(90)
+s.penup()
+s.forward(27)
+s.pendown()
+s.right(90)
+s.color('yellow','yellow')
+s.begin_fill()
+s.circle(75)
+s.end_fill()
+
+
+s.left(90)
+s.forward(20)
+s.color('orange','orange')
+s.right(90)
+s.begin_fill()
+s.circle(55)
+s.end_fill()
+
+s.left(90)
+s.forward(20)
+s.color('maroon','maroon')
+s.right(90)
+s.begin_fill()
+s.circle(35)
+s.end_fill()
+
+s.left(90)
+s.forward(20)
+s.color('pink','pink')
+s.right(90)
+s.begin_fill()
+s.circle(15)
+s.end_fill()
+
+s.penup()
+s.forward(600)
---
+++ Vishruth S/readme.txt
@@ -0,0 +1,3 @@
+Hi. I am Vishruth S from CS1_A
+Here's my final submission for Code_a_Pookkalam 
+Happy Onam!
